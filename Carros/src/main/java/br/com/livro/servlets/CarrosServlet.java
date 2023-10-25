78251811e7e7fe5526e6e267f28c46d59315e42b
ClaytonEduard <clayton_eduard@hotmail.com>
2023-10-25 15:08:34 -0300

    Classes Utils, rodar no java 1.8 e tomcat7 para corrigir erro de Libs

---
+++ Carros/src/main/java/br/com/livro/servlets/CarrosServlet.java
@@ -0,0 +1,37 @@
+package br.com.livro.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.livro.domain.Carro;
+import br.com.livro.domain.CarroService;
+import br.com.livro.domain.ListaCarros;
+import br.com.livro.util.JAXBUtil;
+import br.com.livro.util.ServletUtil;
+
+
+@WebServlet("/carros/*")
+public class CarrosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private CarroService carroService = new CarroService();
+
+	@SuppressWarnings("static-access")
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Carro> carros = carroService.getCarros();
+		ListaCarros lista = new ListaCarros();
+		lista.setCarros(carros);
+		//Gerar o XML
+		JAXBUtil j = new JAXBUtil();
+		String xml = j.toXML(lista);
+		//Escreve o XML na response do Servlet com a APPLICATION/XML
+		ServletUtil.writeXML(resp, xml);
+	}
+
+}
