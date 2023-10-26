86ab9b0aedb807b3e03a6620fab669afd03c23b6
ClaytonEduard <clayton_eduard@hotmail.com>
2023-10-26 11:46:40 -0300

    Implementado GSON para exibição do xml

--- Carros/src/main/java/br/com/livro/servlets/CarrosServlet.java
+++ Carros/src/main/java/br/com/livro/servlets/CarrosServlet.java
@@ -9,12 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import br.com.livro.domain.Carro;
 import br.com.livro.domain.CarroService;
 import br.com.livro.domain.ListaCarros;
 import br.com.livro.util.JAXBUtil;
 import br.com.livro.util.ServletUtil;
-
 
 @WebServlet("/carros/*")
 public class CarrosServlet extends HttpServlet {
@@ -27,16 +29,23 @@ public class CarrosServlet extends HttpServlet {
 		List<Carro> carros = carroService.getCarros();
 		ListaCarros lista = new ListaCarros();
 		lista.setCarros(carros);
-		//Gerar o XML
-		/*JAXBUtil j = new JAXBUtil();
-		String xml = j.toXML(lista);
-		//Escreve o XML na response do Servlet com a APPLICATION/XML
-		ServletUtil.writeXML(resp, xml);*/
-		
+		// Gerar o XML
+		/*
+		 * JAXBUtil j = new JAXBUtil(); String xml = j.toXML(lista); //Escreve o XML na
+		 * response do Servlet com a APPLICATION/XML ServletUtil.writeXML(resp, xml);
+		 */
+
 		// usando o Jettison
-		//Gera o JSON
-		String json = JAXBUtil.toJSON(lista);
-		///Escreve o XML na response do Servlet com a APPLICATION/XML
+		// Gera o JSON
+		/*
+		 * String json = JAXBUtil.toJSON(lista); ///Escreve o XML na response do Servlet
+		 * com a APPLICATION/XML ServletUtil.writeJSON(resp, json);
+		 */
+
+		// Usando o JSON
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(lista);
+		/// Escreve o XML na response do Servlet com a APPLICATION/XML
 		ServletUtil.writeJSON(resp, json);
 	}
 
